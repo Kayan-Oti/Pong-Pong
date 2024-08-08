d4207a743bf7f45e60c4dc5e45576db066c1b47b
Kakohari <kayanoti@gmail.com>
2024-08-07 21:12:02 -0300

    Fix Placar invertido

--- Assets/Scripts/ScoreTrigger.cs
+++ Assets/Scripts/ScoreTrigger.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class ScoreTrigger : MonoBehaviour
 {
-    [SerializeField] private ArenaSide _side;
+    [Tooltip("Chose the side that you want to gain a Point")]
+    [SerializeField] private ArenaSide _sideGettingPoint;
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Ball")){
-            EventManager.MatchManger.OnScoreTrigger.Get().Invoke(this, _side);
+            EventManager.MatchManger.OnScoreTrigger.Get().Invoke(this, _sideGettingPoint);
         }
     }
 }
